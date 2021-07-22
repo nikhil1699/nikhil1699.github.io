19d4bfa646916ec578646c3c729bf4eb0682d4af
nikhil1699 <nikhil.chauhan9916@gmail.com>
2021-07-22 21:18:07 +0530

    css working

--- js/script.js
+++ js/script.js
@@ -21,18 +21,18 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'white.css'
+		document.getElementById('theme-style').href = './css/white.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = './css/blue.css'
 	}
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById('theme-style').href = './css/green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById('theme-style').href = './css/purple.css'
 	}
 
 
